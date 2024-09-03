c2cd06b084d68815982c800a71dc8b1ba29c48b6
ayushipundir13 <ayushi.pundir@godigit.com>
2024-09-03 17:53:13 +0530

    [AYUSHI]

---
+++ src/main/java/com/example/GetIPAdress/repository/SystemInfoRepository.java
@@ -0,0 +1,8 @@
+package com.example.GetIPAdress.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class SystemInfoRepository {
+    // Placeholder for data access logic
+}
